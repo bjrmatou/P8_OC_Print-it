15fb6b81717364014423b6665694f05c5a430c38
bjrmatou <143578923+bjrmatou@users.noreply.github.com>
2023-11-06 16:50:42 +0100

    Version finale du script

--- assets/script.js
+++ assets/script.js
@@ -1,66 +1,89 @@
+// Tableau slides
+// Contient des objets représentant chaque diapo
+// -> Chaque objet a 2 propriétés : 
+//			- "image" (nom du fichier image de la diapo dans les assets img slideshow) 
+//			- "tagLine" (texte associé à la diapo)
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	  "image": "slide1.jpg",
+	  "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	  "image": "slide2.jpg",
+	  "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	  "image": "slide3.jpg",
+	  "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	  "image": "slide4.png",
+	  "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+  ];
 
-
-// EventListener sur les flèches via manipulation du DOM
-
-//Initialisation des variables pour différencier les flèches
-const arrowLeft = document.querySelector(".arrow_left");
-const arrowRight = document.querySelector(".arrow_right");
-
-	// Controle fleche gauche
-	arrowLeft.addEventListener("click", () => {
-		console.log("FLECHE GAUCHE");
-		changeSlider(-1);
-	  });
-	
-	  // Controle fleche droite
-	  arrowRight.addEventListener("click", () => {
-		console.log("FLECHE DROITE");
-		changeSlider(1);
-	  });
-
-
-// Insertion des dots
-const dotsContainer = document.querySelector(".dots"); //création var container récup la div dots
-for (let i = 0; i < slides.length; i++) { // boucle pour definir le nbr de nvlle div a créer
-  const dot = document.createElement("div"); //création variable dot
-  dot.className = "dot";
-  dot.addEventListener("click", () => {
-    console.log("Clic sur le point indicatif " + i);
-    //logique pr changer le dit cliqué
+  // Création des variables pour référencer les éléments de la bannière à manipuler
+  // Elements : Image, Texte et la div contenant les dots
+  const bannerImg = document.querySelector(".banner-img"); 
+  const bannerText = document.querySelector(".banner-text");
+  const dotsContainer = document.querySelector(".dots");
+  
+  // Fonction pour modifier les éléments du DOM
+  function updateSlider(index) {
+	// MAJ image et texte de la bannière
+	bannerImg.src = './assets/images/slideshow/' + slides[index]['image'];
+	bannerText.innerHTML = slides[index]['tagLine'];
+  
+	// MAJ points indicateurs (dots)
+	const dotElements = document.querySelectorAll('.dot');
+	dotElements.forEach((dot, dotIndex) => {
+	  if (dotIndex === index) {
+		// Ajouter la class pour le dot actif
+		dot.classList.add('dot_selected');
+	  } else {
+		// Enlever la class des autres dots
+		dot.classList.remove('dot_selected');
+	  }
+	});
+  }
+  
+  // Initialisation Slider via son index tableau
+  let indexNum = 0;
+  
+  // EventListener sur les flèches
+  const arrowLeft = document.querySelector(".arrow_left");
+  const arrowRight = document.querySelector(".arrow_right");
+  
+  // EventListener pour la flèche gauche
+  arrowLeft.addEventListener("click", () => {
+	// Changer l'index pour passer à la diapositive précédente
+	indexNum = (indexNum - 1 + slides.length) % slides.length;
+	updateSlider(indexNum);
   });
-  dotsContainer.appendChild(dot);
-}
-
-
-// Initialisation Index Slider via son index tableau
-let indexNum = 0;
-
-// Fonction Slider modifications via les param tableau slides, accès via l'index
-function changeSlider(sens) {
-	indexNum += sens;
-	if (indexNum > slides.length -1) 
-		indexNum = 0;
-	if (indexNum < 0)
-		indexNum = slides.length - 1;
-	document.querySelector(".banner-img").src = './assets/images/slideshow/' + slides[indexNum]['image'];
-	document.querySelector(".banner-text").innerHTML = slides[indexNum]['tagLine'];
-	{console.log(sens)}
-}
+  
+  // EventListener pour la flèche droite
+  arrowRight.addEventListener("click", () => {
+	// Changer l'index pour passer à la diapositive suivante
+	indexNum = (indexNum + 1) % slides.length;
+	updateSlider(indexNum);
+  });
+  
+  // Création des points indicateurs (dots) et ajout d'un EventListener
+  if (dotsContainer) {
+	dotsContainer.innerHTML = '';
+	for (let i = 0; i < slides.length; i++) {
+	  const dot = document.createElement('div');
+	  dot.className = 'dot';
+	  dotsContainer.appendChild(dot);
+  
+	  // Ajout EventListener pour passer à la diapositive correspondante lorsqu'on clique sur un point indicateur
+	  dot.addEventListener('click', () => {
+		indexNum = i;
+		updateSlider(indexNum);
+	  });
+	}
+  }
+  
+  // MAJ START slider une fois pour sélectionner le premier point indicateur (dot)
+  updateSlider(indexNum);
+  
